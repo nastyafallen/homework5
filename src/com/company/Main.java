a1ff8cebd7b5566f0dbe435b61432bfed2cf48d8
nastyafallen <nastyafallen@rambler.ru>
2022-04-21 22:49:25 +0300

    Сделанное задание

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,22 +3,64 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// Задание 1
-        int[] massiveOne = new int[] {1, 2, 3};
+        // Задание 1
+        int[] massiveOne = new int[]{1, 2, 3};
 
         double[] massiveTwo = {1.57, 7.654, 9.986};
 
         char[] massiveThree = {'п', 'р', 'и', 'в', 'е', 'т'};
 
         // Задание 2
-        System.out.println(massiveOne[0] + ", " + massiveOne[1] + ", " + massiveOne[2]);
-        System.out.println(massiveTwo[0] + ", " + massiveTwo[1] + ", " + massiveTwo[2]);
-        System.out.println(massiveThree[0] + ", " + massiveThree[1] + ", " + massiveThree[2] + ", " + massiveThree[3] + ", " + massiveThree[4] + ", " + massiveThree[5] + "!");
+        for (int i = 0; i < massiveOne.length; i++) {
+            if (i == massiveOne.length - 1) {
+                System.out.print(massiveOne[i]);
+            } else {
+                System.out.print(massiveOne[i] + ", ");
+            }
+        }
+        System.out.println();
+        for (int a = 0; a < massiveTwo.length; a++) {
+            if (a == massiveTwo.length - 1) {
+                System.out.print(massiveTwo[a]);
+            } else {
+                System.out.print(massiveTwo[a] + ", ");
+            }
+        }
+        System.out.println();
+        for (int c = 0; c < massiveThree.length; c++) {
+            if (c == massiveThree.length - 1) {
+                System.out.print(massiveThree[c]);
+            } else {
+                System.out.print(massiveThree[c] + ", ");
+            }
+        }
+        System.out.println();
 
         // Задание 3
-        System.out.println(massiveOne[2] + ", " + massiveOne[1] + ", " + massiveOne[0]);
-        System.out.println(massiveTwo[2] + ", " + massiveTwo[1] + ", " + massiveTwo[0]);
-        System.out.println(massiveThree[5] + ", " + massiveThree[4] + ", " + massiveThree[3] + ", " + massiveThree[2] + ", " + massiveThree[1] + ", " + massiveThree[0]);
+        for (int i = massiveOne.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.print(massiveOne[i]);
+            } else {
+                System.out.print(massiveOne[i] + ", ");
+            }
+        }
+        System.out.println();
+        for (int a = massiveTwo.length - 1; a >= 0; a--) {
+            if (a == 0) {
+                System.out.print(massiveTwo[a]);
+            } else {
+                System.out.print(massiveTwo[a] + ", ");
+            }
+        }
+        System.out.println();
+        for (int c = massiveThree.length - 1; c >= 0; c--) {
+            if (c == 0) {
+                System.out.print(massiveThree[c]);
+            } else {
+                System.out.print(massiveThree[c] + ", ");
+            }
+        }
+        System.out.println();
 
         // Задание 4
         for (int i = 0; i < massiveOne.length; i++) {
